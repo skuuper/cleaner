c058cb6f77d0de23f01bf1816c8f3fcf8620285a
Martin <martin.verrev@gmail.com>
2016-09-09 15:08:40 +0300

    Updated NLTK wrapper

--- app/Service/NltkService.php
+++ app/Service/NltkService.php
@@ -14,39 +14,39 @@ namespace App\Service;
 class NltkService
 {
     private $debug;
+    private $api_file;
 
     public function __construct($debug = 0)
     {
+        //TODO: Move API file location to external configuration
         $this->debug = $debug;
+        $this->api_file = "/home/martin/work/modules/run_api.py";
     }
 
 
     private function call_py_api($method, $text)
     {
         $json = json_encode(array($text));
-        $result = shell_exec('python ../run_api.py ' . $method . ' ' . escapeshellarg($json) . ' 2>&1');
+        $result = shell_exec('python ' . $this->api_file . '  '. $method . ' ' . escapeshellarg($json) . ' 2>&1');
+        dd($result);
         $resultData = join("\n", json_decode($result, true)['result']);
         return $resultData;
     }
 
-    public function chunk_text($text)
-    {
-        return call_py_api('chunk', $text);
+    public function chunk_text($text) {
+        return $this->call_py_api('chunk', $text);
     }
 
-    public function ner_text($text)
-    {
-        return call_py_api('ner', $text);
+    public function ner_text($text) {
+        return $this->call_py_api('ner', $text);
     }
 
-    public function detect_lang($text)
-    {
-        return call_py_api('detectlang', $text);
+    public function detect_lang($text) {
+        return $this->call_py_api('detectlang', $text);
     }
 
-    public function prepare($text)
-    {
-        return call_py_api('prepare', $text);
+    public function prepare($text) {
+        return $this->call_py_api('prepare', $text);
     }
 
 }
