3ac83da5231aa7b9ff92afe839fabd839f429817
NetBUG <oleg@skuuper.com>
2017-05-30 15:57:15 -0700

    Correct and full Russian lemmatization

--- thirdparty/aligner_ch/bin/axis.RU/lemmatizer.py
+++ thirdparty/aligner_ch/bin/axis.RU/lemmatizer.py
@@ -1,1 +1,30 @@
-/Users/ourzhumt/projects/Marat_th/ZhRuParCor_pipeline/ZhRuParCor/lemmatizer.py
+#!/usr/bin/env python3
+#coding=utf-8
+# @author Maratych
+# @description Lemmatizing Russian in clear cases (single analysis)
+
+import pymorphy2
+import sys
+morph = pymorphy2.MorphAnalyzer()
+from nltk import word_tokenize
+
+line = 'Я люблю китайско-русский параллельный корпус НКРЯ!'
+
+def normal_form(line):
+    tokens = word_tokenize(line)
+    lemmas = []
+    for token in tokens:
+        token = token.lower().strip()
+        stems = [p.normal_form for p in morph.parse(token)]
+        if len(set(stems)) == 1:
+            lemmas.append(stems[0])
+        else:
+            #print ('non-unique word ' + token + ": " + repr(set(stems)))
+            #print(repr(morph.parse(token)))
+            lemmas.append(token)
+    new_line = (' ').join(lemmas)
+    return new_line
+
+if __name__ == '__main__':
+    print(normal_form(line))
+
